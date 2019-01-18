05a107370dee34c1dbcd95a203c14563abeaf81b
Kevin Cheng <kevinx1@gmail.com>
2019-01-18 14:54:11 -0800

    Merge updates from original repo

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 #define working directory
-<<<<<<< HEAD
 work_dir="/home/kevincheng/Git/garage_benchmarks/"
-=======
-#this needs to be updated per agent.
-work_dir="/usr/git/garage/"
-
->>>>>>> 03c490d50d6e0bfbe595f46af8ef9b425d617068
 #pull repo to working directory
 repo="https://github.com/rlworkgroup/garage.git"
 branch="automated-benchmarking"
@@ -21,7 +15,6 @@ git checkout "$branch"
 
 docker build . -t garage-ci -f docker/Dockerfile.ci --build-arg MJKEY="$(cat ~/.mujoco/mjkey.txt)"
 docker stop garage_benchmark
-
 docker rm garage_benchmark
 docker run --name garage_benchmark -e MJKEY="$(cat ~/.mujoco/mjkey.txt)" garage-ci nose2 -c setup.cfg "$test_file"
 echo "done running benchmarks in docker"
@@ -35,15 +28,10 @@ echo "done copying results"
 jsonfile="$work_dir/docs/resources/progress.json"
 if [ -f "$jsonfile" ]
 then
-<<<<<<< HEAD
     git add "$jsonfile"
-=======
-    git add -A "$jsonfile"
->>>>>>> 03c490d50d6e0bfbe595f46af8ef9b425d617068
     git commit -m "update progress json"
     git push
 else
     echo "progress.json not found"
 fi 
-
 
